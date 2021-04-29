56c3b7d5e1fee76da29db3f03bdf10f3d6155f4a
Mohamed Khalil <mohamed.khalil@outlook.com>
2021-04-29 12:30:06 +0100

    Added the theme

--- src/core/layout/App.tsx
+++ src/core/layout/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Cats from '../../features/Cats';
 import ImageUploadForm from "../../features/upload-image/image-upload-form";
+import Shell from './shell';
+import "tailwindcss/tailwind.css"
 
 function App() {
-  return (
-    <div>
-      <ImageUploadForm/>
-      <Cats/>
-    </div>
-  );
+    return (
+        <div>
+            <Shell/>
+            <ImageUploadForm/>
+            <Cats/>
+        </div>
+    );
 }
 
 export default App;
