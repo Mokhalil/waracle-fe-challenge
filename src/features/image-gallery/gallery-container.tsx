5754117d0a9304bd9a0ee14883409eae3b366535
Mohamed Khalil <mohamed.khalil@outlook.com>
2021-04-30 08:40:21 +0100

    Code refactoring

--- src/features/image-gallery/gallery-container.tsx
+++ src/features/image-gallery/gallery-container.tsx
@@ -2,96 +2,17 @@ import React, {Fragment} from 'react';
 import Gallery from "./gallery";
 import {useGallery} from "./use-gallery-hook";
 import {observer} from "mobx-react-lite";
+import Header from "../common/Header";
 
 const GalleryContainer = observer(() => {
-    const {images,toggleAsFavourite, onVoteDown,onVoteUp} = useGallery()
+
+    // All event handlers or utilities are encapsulated in useGallery hook.
+    const {images, toggleAsFavourite, onVoteDown, onVoteUp} = useGallery()
+
     return (
         <Fragment>
-            <div className="flex">
-                <h1 className="flex-1 text-2xl font-bold text-gray-900">Photo Gallery</h1>
-                <div className="ml-6 bg-gray-100 p-0.5 rounded-lg flex items-center sm:hidden">
-                    <button type="button"
-                            className="p-1.5 rounded-md text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        {/* Heroicon name: solid/view-list */}
-                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fillRule="evenodd"
-                                  d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                  clipRule="evenodd"/>
-                        </svg>
-                        <span className="sr-only">Use list view</span>
-                    </button>
-                    <button type="button"
-                            className="ml-0.5 bg-white p-1.5 rounded-md shadow-sm text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        {/* Heroicon name: solid/view-grid */}
-                        <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path
-                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"/>
-                        </svg>
-                        <span className="sr-only">Use grid view</span>
-                    </button>
-                </div>
-            </div>
-            {/* Tabs */}
-            <div className="mt-3 sm:mt-2">
-                <div className="sm:hidden">
-                    <label htmlFor="tabs" className="sr-only">Select a tab</label>
-                    <select id="tabs" name="tabs"
-                            className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                        <option selected>Recently Viewed</option>
-                        <option>Recently Added</option>
-                        <option>Favorited</option>
-                    </select>
-                </div>
-                <div className="hidden sm:block">
-                    <div className="flex items-center border-b border-gray-200">
-                        <nav className="flex-1 -mb-px flex space-x-6 xl:space-x-8"
-                             aria-label="Tabs">
-                            {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
-                            <a href="#" aria-current="page"
-                               className="border-indigo-500 text-indigo-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
-                                Recently Viewed
-                            </a>
-                            <a href="#"
-                               className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
-                                Recently Added
-                            </a>
-                            <a href="#"
-                               className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
-                                Favorited
-                            </a>
-                        </nav>
-                        <div
-                            className="hidden ml-6 bg-gray-100 p-0.5 rounded-lg items-center sm:flex">
-                            <button type="button"
-                                    className="p-1.5 rounded-md text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                {/* Heroicon name: solid/view-list */}
-                                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fillRule="evenodd"
-                                          d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                          clipRule="evenodd"/>
-                                </svg>
-                                <span className="sr-only">Use list view</span>
-                            </button>
-                            <button type="button"
-                                    className="ml-0.5 bg-white p-1.5 rounded-md shadow-sm text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                {/* Heroicon name: solid/view-grid */}
-                                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path
-                                        d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"/>
-                                </svg>
-                                <span className="sr-only">Use grid view</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <Gallery items={images} onVoteUp={onVoteUp} onVoteDown={onVoteDown} onToggleFavourite={toggleAsFavourite} />
+            <Header title={'Photo Gallery'}/>
+            <Gallery items={images} onVoteUp={onVoteUp} onVoteDown={onVoteDown} onToggleFavourite={toggleAsFavourite}/>
         </Fragment>
     );
 });
